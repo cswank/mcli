198dbc30c37262ca8277d35674c8175dfa0112c4
Craig Swank <craigswank@gmail.com>
2018-03-15 07:39:43 -0600

    moved download logic from queue to flac

--- internal/player/flac.go
+++ internal/player/flac.go
@@ -1,8 +1,13 @@
 package player
 
 import (
+	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/cswank/beep"
@@ -13,17 +18,18 @@ import (
 
 type Flac struct {
 	Fetcher
-	queue            *queue
-	source           Fetcher
-	history          History
-	playing          bool
-	pause            chan bool
-	vol              chan float64
-	fastForward      chan bool
-	playCB           func(Progress)
-	downloadProgress chan Progress
-	downloadCB       func(Progress)
-	nextSong         func(r Result)
+	queue        *queue
+	history      History
+	playing      bool
+	sep          string
+	pause        chan bool
+	vol          chan float64
+	fastForward  chan bool
+	playCB       func(Progress)
+	downloadCB   func(Progress)
+	nextSong     func(r Result)
+	onDeck       chan Result
+	onDeckResult *Result
 }
 
 func newFlac(f Fetcher) (*Flac, error) {
@@ -32,23 +38,18 @@ func newFlac(f Fetcher) (*Flac, error) {
 		return nil, err
 	}
 
-	dlp := make(chan Progress)
-	q, err := newQueue(f.Name(), f.GetTrack, dlp)
-	if err != nil {
-		return nil, err
-	}
-
 	p := &Flac{
-		Fetcher:          f,
-		history:          hist,
-		queue:            q,
-		pause:            make(chan bool),
-		fastForward:      make(chan bool),
-		vol:              make(chan float64),
-		downloadProgress: dlp,
+		Fetcher:     f,
+		history:     hist,
+		sep:         string(filepath.Separator),
+		queue:       newQueue(),
+		pause:       make(chan bool),
+		fastForward: make(chan bool),
+		vol:         make(chan float64),
+		onDeck:      make(chan Result),
 	}
 
-	go p.loop()
+	go p.playLoop()
 	go p.downloadLoop()
 	return p, nil
 }
@@ -58,7 +59,7 @@ func (f *Flac) NextSong(fn func(Result)) {
 }
 
 func (f *Flac) Play(r Result) {
-	f.queue.add(r)
+	f.queue.Add(r)
 }
 
 func (f *Flac) History(page, pageSize int) (*Results, error) {
@@ -84,11 +85,17 @@ func (f *Flac) Volume(v float64) {
 }
 
 func (f *Flac) Queue() []Result {
-	return f.queue.playlist()
+	var out []Result
+	if f.onDeckResult != nil {
+		out = []Result{*f.onDeckResult}
+	}
+	q := f.queue.Playlist()
+	return append(out, q...)
 }
 
 func (f *Flac) RemoveFromQueue(i int) {
-	f.queue.remove(i)
+	//TODO: allow removing of onDeckResult
+	f.queue.Remove(i - 1)
 }
 
 func (f *Flac) FastForward() {
@@ -99,16 +106,17 @@ func (f *Flac) FastForward() {
 
 func (f *Flac) downloadLoop() {
 	for {
-		prog := <-f.downloadProgress
-		if f.downloadCB != nil {
-			f.downloadCB(prog)
-		}
+		r := f.queue.Next()
+		f.onDeckResult = &r
+		f.download(&r)
+		f.onDeck <- r
+		f.onDeckResult = nil
 	}
 }
 
-func (f *Flac) loop() {
+func (f *Flac) playLoop() {
 	for {
-		r := f.queue.next()
+		r := <-f.onDeck
 		if f.nextSong != nil {
 			f.nextSong(r)
 		}
@@ -185,4 +193,97 @@ func (f *Flac) DownloadProgress(fn func(Progress)) {
 
 func (f *Flac) PlayProgress(fn func(Progress)) {
 	f.playCB = fn
+}
+
+func (f *Flac) download(r *Result) {
+	pth, e := f.checkCache(*r)
+	log.Println("check cache", pth, e)
+	r.Path = pth
+	if !e {
+		err := f.doDownload(*r)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func (f *Flac) doDownload(r Result) error {
+	file, err := os.Create(r.Path)
+	if err != nil {
+		return fmt.Errorf("could not create file for %+v: %s", r, err)
+	}
+
+	u, err := f.Fetcher.GetTrack(r.Track.ID)
+	if err != nil {
+		return fmt.Errorf("could not get track %+v: %s", r, err)
+	}
+
+	resp, err := http.Get(u)
+	if err != nil {
+		return fmt.Errorf("could not get stream %+v: %s", r, err)
+	}
+
+	pr := newProgressRead(resp.Body, int(resp.ContentLength), f.downloadCB)
+	_, err = io.Copy(file, pr)
+	if err != nil {
+		return err
+	}
+
+	file.Close()
+	pr.Close()
+	return nil
+}
+
+func (f *Flac) ensureCache() error {
+	dir := fmt.Sprintf("%s/cache/%s", os.Getenv("MCLI_HOME"), f.Fetcher.Name())
+	e, _ := exists(dir)
+	if e {
+		return nil
+	}
+	return os.MkdirAll(dir, 0700)
+}
+
+func (f *Flac) checkCache(result Result) (string, bool) {
+	dir := fmt.Sprintf("%s/cache/%s/%s/%s", os.Getenv("MCLI_HOME"), f.Fetcher.Name(), f.clean(result.Artist.Name), f.clean(result.Album.Title))
+	e, _ := exists(dir)
+	if !e {
+		os.MkdirAll(dir, 0700)
+	}
+
+	pth := fmt.Sprintf("%s/cache/%s/%s/%s/%s.flac", os.Getenv("MCLI_HOME"), f.Fetcher.Name(), f.clean(result.Artist.Name), f.clean(result.Album.Title), f.clean(result.Track.Title))
+	e, _ = exists(pth)
+	return pth, e
+}
+
+func (f *Flac) clean(s string) string {
+	return strings.Replace(s, f.sep, "", -1)
+}
+
+type progressRead struct {
+	io.Reader
+	t, l, reads int
+	cb          func(Progress)
+}
+
+func newProgressRead(r io.Reader, l int, cb func(Progress)) *progressRead {
+	return &progressRead{Reader: r, t: 0, l: l, cb: cb}
+}
+
+func (r *progressRead) Read(p []byte) (int, error) {
+	n, err := r.Reader.Read(p)
+	r.t += n
+	r.reads++
+	if r.reads%100 == 0 {
+		r.cb(Progress{N: r.t, Total: r.l})
+	}
+	return n, err
+}
+
+// Close the reader when it implements io.Closer
+func (r *progressRead) Close() error {
+	r.cb(Progress{N: 0, Total: r.t})
+	if closer, ok := r.Reader.(io.Closer); ok {
+		return closer.Close()
+	}
+	return nil
 }
--- internal/player/queue.go
+++ internal/player/queue.go
@@ -1,15 +1,7 @@
 package player
 
 import (
-	"fmt"
-	"io"
-	"log"
-	"net/http"
-	"os"
-	"path/filepath"
-	"strings"
 	"sync"
-	"time"
 )
 
 type Progress struct {
@@ -18,42 +10,27 @@ type Progress struct {
 }
 
 type queue struct {
-	sourceName string
-	in         []Result
-	queue      []Result
-	out        chan Result
-	lock       sync.Mutex
-	outLock    sync.Mutex
-	sep        string
-	downloadCh chan<- Progress
-	getTrack   func(string) (string, error)
+	queue []Result
+	lock  sync.Mutex
+	ready chan bool
 }
 
-func newQueue(name string, getTrack func(string) (string, error), buf chan<- Progress) (*queue, error) {
-	q := &queue{
-		getTrack:   getTrack,
-		downloadCh: buf,
-		sourceName: name,
-		sep:        string(filepath.Separator),
-		out:        make(chan Result),
+func newQueue() *queue {
+	return &queue{
+		ready: make(chan bool),
 	}
-
-	if err := q.ensureCache(); err != nil {
-		return nil, err
-	}
-
-	go q.download()
-	return q, nil
 }
 
-func (q *queue) add(r Result) {
+func (q *queue) Add(r Result) {
 	q.lock.Lock()
-	q.in = append(q.in, r)
 	q.queue = append(q.queue, r)
 	q.lock.Unlock()
+	go func() {
+		q.ready <- true
+	}()
 }
 
-func (q *queue) remove(i int) {
+func (q *queue) Remove(i int) {
 	q.lock.Lock()
 	defer q.lock.Unlock()
 	if len(q.queue) == 0 || i >= len(q.queue) {
@@ -62,124 +39,21 @@ func (q *queue) remove(i int) {
 	q.queue = append(q.queue[:i], q.queue[i+1:]...)
 }
 
-func (q *queue) playlist() []Result {
+func (q *queue) Playlist() []Result {
 	q.lock.Lock()
 	defer q.lock.Unlock()
 	return q.queue
 }
 
-func (q *queue) next() Result {
-	r := <-q.out
-	q.lock.Lock()
-	if len(q.queue) > 0 {
-		q.queue = q.queue[1:]
+func (q *queue) Next() Result {
+	<-q.ready
+	if len(q.queue) == 0 {
+		return q.Next()
 	}
+
+	q.lock.Lock()
+	r := q.queue[0]
+	q.queue = q.queue[1:]
 	q.lock.Unlock()
 	return r
-}
-
-func (q *queue) download() {
-	for {
-		time.Sleep(time.Second)
-		q.lock.Lock()
-		if len(q.in) == 0 {
-			q.lock.Unlock()
-			continue
-		}
-
-		r := q.in[len(q.in)-1]
-		q.in = q.in[:len(q.in)-1]
-		q.lock.Unlock()
-		pth, e := q.checkCache(r)
-		r.Path = pth
-		if !e {
-			err := q.doDownload(r)
-			if err != nil {
-				log.Println(err)
-				continue
-			}
-		}
-		q.out <- r
-	}
-}
-
-func (q *queue) doDownload(r Result) error {
-	f, err := os.Create(r.Path)
-	if err != nil {
-		return fmt.Errorf("could not create file for %+v: %s", r, err)
-	}
-
-	u, err := q.getTrack(r.Track.ID)
-	if err != nil {
-		return fmt.Errorf("could not get track %+v: %s", r, err)
-	}
-
-	resp, err := http.Get(u)
-	if err != nil {
-		return fmt.Errorf("could not get stream %+v: %s", r, err)
-	}
-
-	pr := newProgressRead(resp.Body, int(resp.ContentLength), q.downloadCh)
-	_, err = io.Copy(f, pr)
-	if err != nil {
-		return err
-	}
-
-	f.Close()
-	pr.Close()
-	return nil
-}
-
-func (q *queue) ensureCache() error {
-	dir := fmt.Sprintf("%s/cache/%s", os.Getenv("MCLI_HOME"), q.sourceName)
-	e, _ := exists(dir)
-	if e {
-		return nil
-	}
-	return os.MkdirAll(dir, 0700)
-}
-
-func (q *queue) checkCache(result Result) (string, bool) {
-	dir := fmt.Sprintf("%s/cache/%s/%s/%s", os.Getenv("MCLI_HOME"), q.sourceName, q.clean(result.Artist.Name), q.clean(result.Album.Title))
-	e, _ := exists(dir)
-	if !e {
-		os.MkdirAll(dir, 0700)
-	}
-
-	pth := fmt.Sprintf("%s/cache/%s/%s/%s/%s.flac", os.Getenv("MCLI_HOME"), q.sourceName, q.clean(result.Artist.Name), q.clean(result.Album.Title), q.clean(result.Track.Title))
-	e, _ = exists(pth)
-	return pth, e
-}
-
-func (q *queue) clean(s string) string {
-	return strings.Replace(s, q.sep, "", -1)
-}
-
-type progressRead struct {
-	io.Reader
-	t, l, reads int
-	ch          chan<- Progress
-}
-
-func newProgressRead(r io.Reader, l int, ch chan<- Progress) *progressRead {
-	return &progressRead{Reader: r, t: 0, l: l, ch: ch}
-}
-
-func (r *progressRead) Read(p []byte) (int, error) {
-	n, err := r.Reader.Read(p)
-	r.t += n
-	r.reads++
-	if r.reads%100 == 0 {
-		r.ch <- Progress{N: r.t, Total: r.l}
-	}
-	return n, err
-}
-
-// Close the reader when it implements io.Closer
-func (r *progressRead) Close() error {
-	r.ch <- Progress{N: 0, Total: r.t}
-	if closer, ok := r.Reader.(io.Closer); ok {
-		return closer.Close()
-	}
-	return nil
 }
