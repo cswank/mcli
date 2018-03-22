a127d7fdcd2587ada34d32f9683baf1c2ce5a7ff
Craig Swank <craigswank@gmail.com>
2018-03-22 10:48:45 -0600

    keep track of count

--- internal/player/storm.go
+++ internal/player/storm.go
@@ -10,6 +10,7 @@ import (
 
 type StormEntry struct {
 	ID     string `storm:"id"`
+	Count  int    `storm:"index"`
 	Time   string `storm:"index"`
 	Result Result
 }
@@ -32,14 +33,14 @@ func (b *StormHistory) Save(r Result) error {
 	var entry StormEntry
 	err := b.db.One("ID", r.Track.ID, &entry)
 	if err == storm.ErrNotFound {
-		return b.db.Save(&StormEntry{ID: r.Track.ID, Time: time.Now().Format(time.RFC3339), Result: r})
+		return b.db.Save(&StormEntry{ID: r.Track.ID, Count: 1, Time: time.Now().Format(time.RFC3339), Result: r})
 	}
 
 	if err != nil {
 		return err
 	}
 
-	return b.db.Update(&StormEntry{ID: r.Track.ID, Time: time.Now().Format(time.RFC3339), Result: r})
+	return b.db.Update(&StormEntry{ID: r.Track.ID, Count: entry.Count + 1, Time: time.Now().Format(time.RFC3339), Result: r})
 }
 
 func (b *StormHistory) Fetch(page, pageSize int) (*Results, error) {
